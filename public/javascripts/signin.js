ee99a522101d78b5ef10112fe5d7ee5edbabdc75
Jin <khj9709@icloud.com>
2022-07-06 22:12:58 +0900

    feat: :sparkles: 빈 값 인식 후 빨간 텍스트 경고하기 #4

---
+++ public/javascripts/signin.js
@@ -0,0 +1,19 @@
+import { $ } from "./utils/easy-selector.js";
+
+const form = $(".signinPage__form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const target = e.target;
+  const [email, pwd] = target;
+
+  if (email.value.trim() === "") email.classList.add("error");
+  if (pwd.value.trim() === "") pwd.classList.add("error");
+
+  if (email.value.trim() !== "" && pwd.value.trim() !== "") {
+    /**
+     * TODO :: 로그인 구현하기
+     */
+  }
+});
