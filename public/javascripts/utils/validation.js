896e211a603fa2c0193c6f1a7ab917f80826e6e5
Jin <khj9709@icloud.com>
2022-07-08 02:31:29 +0900

    feat: :sparkles: 비밀번호 validation #10

--- public/javascripts/utils/validation.js
+++ public/javascripts/utils/validation.js
@@ -1,7 +1,19 @@
+const uppercaseExp = /([a-z])/;
+const lowercaseExp = /([A-Z])/;
+const etcExp = /([\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"])/;
+const numberExp = /([0-9])/;
+const exps = [uppercaseExp, lowercaseExp, etcExp, numberExp];
+
+export const errorMessages = {
+  비밀번호:
+    "10자 이상, 대문자, 소문자, 숫자, 특수문자 중 2종류를 조합해야해요.",
+  생년월일: "날짜 형식이 잘못되었어요. (YYYY-MM-DD)",
+};
+
 export const getIsPhoneValid = (phoneNum) => {
   return (
     typeof phoneNum === "string" &&
-    phoneNum.length === 13 &&
+    !phoneNum.length === 13 &&
     /^010-[0-9]{4}-[0-9]{4}$/.test(phoneNum)
   );
 };
@@ -14,4 +26,20 @@ export const getIsEmailValid = (email) => {
   return (
     typeof email === "string" && /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
   );
+};
+
+export const getIsPasswordValid = (password) => {
+  if (typeof password !== "string") return false;
+
+  if (password.length < 10) return false;
+
+  const passwordExpType = exps.reduce((acc, cur) => {
+    if (password.search(cur) !== -1) return acc + 1;
+
+    return acc;
+  }, 0);
+
+  if (passwordExpType < 2) return false;
+
+  return true;
 };
